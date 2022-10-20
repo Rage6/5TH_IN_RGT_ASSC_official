44f2e2b4b48e69536fa1d1a60421318893ceca5a
Nicholas Vogt <nicholas.vogt2017@gmail.com>
2022-10-20 00:56:16 -0400

    testing Vue on welcome and home

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -5,7 +5,7 @@
                 <div class="card">
                     <div class="card-header">Example Component</div>
 
-                    <div class="card-body" style="color:white">
+                    <div class="card-body">
                         I'm an example component.
                     </div>
                 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,7 @@
                 </div>
             </div>
         </nav>
-
+        <example-component></example-component>
         <main class="py-4">
             @yield('content')
         </main>
