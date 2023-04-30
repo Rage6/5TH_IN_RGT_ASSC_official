18c0669ac8f2983d22ebbb99cc9ec32d175f912b
Nicholas Vogt <nicholas.vogt2017@gmail.com>
2023-04-30 14:00:38 -0400

    fixed menu dropdown on Dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,10 +15,16 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <!-- Old jQuery -->
     <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> -->
+
+    <!-- Needed this to make the menu dropdown work -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- From public/js -->
     <script src="{{ asset(config('app.url_ext').'js/app.js') }}" defer></script>
+
+    <!-- Personally added JS -->
     <script src="{{ asset(config('app.url_ext').'js/my_custom/home/home.js') }}" defer></script>
     <script src="{{ asset(config('app.url_ext').'js/my_custom/home/admin.js') }}" defer></script>
-    <!-- Personally added JS -->
 
     <!-- Fonts -->
 
